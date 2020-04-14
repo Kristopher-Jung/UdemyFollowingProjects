d3e1fa5c69c654ba3e655de079fa62ed7ae6ad7a
Kristopher Jung <kristopher.jung@gm.com>
2020-04-14 17:59:47 -0400

    Redux Tutorial

--- it-logger-redux/src/components/logs/Logs.js
+++ it-logger-redux/src/components/logs/Logs.js
@@ -1,40 +1,41 @@
-import React, { useState, useEffect } from "react";
+import React, { useEffect } from "react";
+import { connect } from "react-redux";
 import LogItem from "./LogItem";
 import Preloader from "../layout/Preloader";
+import PropTypes from "prop-types";
+import { getLogs } from "../../actions/logActions";
 
-const Logs = () => {
-	const [logs, setLogs] = useState([]);
-	const [loading, setLoading] = useState(false);
+const Logs = ({ log: { logs, loading }, getLogs }) => {
+  useEffect(() => {
+    getLogs();
+    // eslint-disable-next-line
+  }, []);
 
-	useEffect(() => {
-		getLogs();
-		// eslint-disable-next-line
-	}, []);
+  if (loading || logs === null) {
+    return <Preloader></Preloader>;
+  }
 
-	const getLogs = async () => {
-		setLoading(true);
-		const res = fetch("/logs");
-		const data = await (await res).json();
-		setLogs(data);
-		setLoading(false);
-	};
-
-	if (loading) {
-		return <Preloader></Preloader>;
-	}
-
-	return (
-		<ul className='collection width-header'>
-			<li className='collection-header'>
-				<h4 className='center'>System Logs</h4>
-			</li>
-			{!loading && logs.length === 0 ? (
-				<p className='center'>No logs to show...</p>
-			) : (
-				logs.map((log) => <LogItem log={log} key={log.id} />)
-			)}
-		</ul>
-	);
+  return (
+    <ul className='collection width-header'>
+      <li className='collection-header'>
+        <h4 className='center'>System Logs</h4>
+      </li>
+      {!loading && logs.length === 0 ? (
+        <p className='center'>No logs to show...</p>
+      ) : (
+        logs.map((log) => <LogItem log={log} key={log.id} />)
+      )}
+    </ul>
+  );
 };
 
-export default Logs;
+Logs.propTypes = {
+  log: PropTypes.object.isRequired,
+  getLogs: PropTypes.func.isRequired,
+};
+
+const mapStateToProps = (state) => ({
+  log: state.log,
+});
+
+export default connect(mapStateToProps, { getLogs })(Logs);
