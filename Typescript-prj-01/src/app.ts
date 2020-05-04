6a4ae4a89ab874f14cb224497df49408d8277c84
Kristopher Jung <nhj940913@gmail.com>
2020-05-03 23:23:27 -0400

    Typescript tutorials

---
+++ Typescript-prj-01/src/app.ts
@@ -0,0 +1,91 @@
+// autobind decorator
+function autobind(_: any, _2: string, descriptor: PropertyDescriptor) {
+	const originalMethod = descriptor.value;
+	const adjustedDescriptor: PropertyDescriptor = {
+		configurable: true,
+		get() {
+			const boundFn = originalMethod.bind(this);
+			return boundFn;
+		},
+	};
+	return adjustedDescriptor;
+}
+
+class ProjectInput {
+	templateElement: HTMLTemplateElement;
+	hostElement: HTMLDivElement;
+	element: HTMLFormElement;
+	titleInputElement: HTMLInputElement;
+	DescriptionInputElement: HTMLInputElement;
+	PeopleInputElement: HTMLInputElement;
+
+	//constructor
+	constructor() {
+		this.templateElement = document.getElementById(
+			"project-input"
+		)! as HTMLTemplateElement;
+		this.hostElement = document.getElementById("app")! as HTMLDivElement;
+
+		const importedNode = document.importNode(
+			this.templateElement.content,
+			true
+		);
+		this.element = importedNode.firstElementChild as HTMLFormElement;
+		this.element.id = "user-input";
+		this.titleInputElement = this.element.querySelector(
+			"#title"
+		) as HTMLInputElement;
+		this.DescriptionInputElement = this.element.querySelector(
+			"#description"
+		) as HTMLInputElement;
+		this.PeopleInputElement = this.element.querySelector(
+			"#people"
+		) as HTMLInputElement;
+		this.configure();
+		this.attach();
+	}
+
+	private gatherUserInput(): [string, string, number] | void {
+		const enteredTitle = this.titleInputElement.value;
+		const enteredDescription = this.DescriptionInputElement.value;
+		const enteredPeople = this.PeopleInputElement.value;
+
+		if (
+			enteredTitle.trim().length === 0 ||
+			enteredDescription.trim().length === 0 ||
+			enteredPeople.trim().length === 0
+		) {
+			alert("Invalid input, please try again!");
+			return;
+		} else {
+			return [enteredTitle, enteredDescription, +enteredPeople];
+		}
+	}
+
+	@autobind
+	private submitHandler(event: Event) {
+		event.preventDefault();
+		const userInput = this.gatherUserInput();
+		if (Array.isArray(userInput)) {
+			const [title, desc, people] = userInput;
+			console.log(title, desc, people);
+		}
+		this.clearInputs();
+	}
+
+	private clearInputs() {
+		this.titleInputElement.value = "";
+		this.DescriptionInputElement.value = "";
+		this.PeopleInputElement.value = "";
+	}
+
+	private configure() {
+		this.element.addEventListener("submit", this.submitHandler);
+	}
+
+	private attach() {
+		this.hostElement.insertAdjacentElement("afterbegin", this.element);
+	}
+}
+
+const prjInput = new ProjectInput();
